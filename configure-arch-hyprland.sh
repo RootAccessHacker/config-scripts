092a3231d0f9c97fe24ec0a04f6af5670a76baee
Cerberus <myrmidon@protonmail.ch>
2024-03-06 03:18:30 +0100

    Update configure-arch-hyprland.sh

--- configure-arch-hyprland.sh
+++ configure-arch-hyprland.sh
@@ -1,8 +1,21 @@
 # Refresh the package cache and update the system 
 sudo pacman -Syu
 
+# Install VMWare Support
+sudo pacman -S open-vm-tools
+
+# Install prerequisites if not installed (--needed)
 sudo pacman -S --needed base-devel git
 
+# Build yay package
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+
+cd ..
+
+# Install and run hyprland config
+git clone https://github.com/SolDoesTech/hyprland.git
+cd hyprland
+chmod +x set-hypr
+./set-hypr
